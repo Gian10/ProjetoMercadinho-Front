d0ad330be8597dcf085505ac028cd84ecebdb62a
Gian10 <gianaraujo@hotmail.com>
2020-12-28 10:24:49 -0300

    ajustes em usuario

--- src/app/tela-principal/tela-principal.component.ts
+++ src/app/tela-principal/tela-principal.component.ts
@@ -1,21 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import {UsuarioService} from '../services/usuario-service'
+import {Usuario} from '../model/usuario-model'
 
 @Component({
   selector: 'app-tela-principal',
   templateUrl: './tela-principal.component.html',
-  styleUrls: ['./tela-principal.component.css']
+  styleUrls: ['./tela-principal.component.css'],
+  providers: [UsuarioService]
 })
 export class TelaPrincipalComponent implements OnInit {
 
   public nameUser : string
   public idUser : number
 
-  constructor() { }
+  constructor(private userService : UsuarioService) { }
 
   ngOnInit(): void {
     let id = window.localStorage.getItem('idUser')
     this.idUser = Number(id)
-    this.nameUser = window.localStorage.getItem('nameUser')
+    this.userService.getUsuarioId(this.idUser)
+    .then((user : Usuario)=>{
+      this.nameUser = user[0].nome
+    })
   }
 
   public logout(){
